1201a59130ee6f04398dfb5381c79914460d905b
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-01 22:08:51 +0800

    Add files via upload

--- packages/opera.rb
+++ packages/opera.rb
@@ -36,15 +36,20 @@ class Opera < Package
   def self.install
     # llvm-strip doesn't works with opera
     ENV['CREW_NOT_STRIP'] = '1'
-    FileUtils.mkdir_p CREW_DEST_PREFIX
-    FileUtils.mv Dir.glob('usr/*'), CREW_DEST_PREFIX
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/opera", "#{CREW_DEST_PREFIX}/bin/x-www-browser" unless File.exist?("#{CREW_PREFIX}/bin/x-www-browser")
     
     if ARCH == 'x86_64' then
       # since opera put the executable to library, we need to link it to CREW_PREFIX
       # remove old symlink which link to /usr/lib/x86_64-linux-gnu/opera/opera
-      FileUtils.rm 'usr/bin/opera'
-      FileUtils.ln_s "#{CREW_LIB_PREFIX}/x86_64-linux-gnu/opera/opera", "#{CREW_DEST_PREFIX}/bin/opera"
+      FileUtils.rm_rf 'bin/opera'
+      FileUtils.ln_s "#{CREW_LIB_PREFIX}/opera/opera", 'bin/opera'
+      
+      # place all stuff to lib64 instead of lib
+      FileUtils.mv 'lib/x86_64-linux-gnu/', 'lib64/'
+      FileUtils.rm_rf 'lib/'
     end
+    
+    FileUtils.mkdir_p CREW_DEST_PREFIX
+    FileUtils.mv Dir.glob('*'), CREW_DEST_PREFIX
+    FileUtils.ln_s "#{CREW_PREFIX}/bin/opera", "#{CREW_DEST_PREFIX}/bin/x-www-browser" unless File.exist?("#{CREW_PREFIX}/bin/x-www-browser")
   end
 end
